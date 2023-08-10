b7858e019dbae66c5af2dcea595271c8540bc37b
chatinunk97 <chatinun.hobby@gmail.com>
2023-08-10 13:06:29 +0700

    Leap Year + Age cal optimize

--- 04-Arrow-Function/10-calAge/main.js
+++ 04-Arrow-Function/10-calAge/main.js
@@ -1,2 +1,16 @@
-let calAge = year => (year - 2020)*365;
-console.log(calAge(2022));
+let isleapYear = (year) => {
+    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+  };
+let calAge = year => {
+    result = 0;
+    for( year ; year <= 2020 ; year++){
+        if(isleapYear(year)){
+            result += 366;
+        }
+        else{
+            result += 365;
+        }
+    }
+    return result;
+};
+console.log(calAge(1997));
